d474c55d08e1538c6b4cca2635bdcd62c6283118
gnanam6931 <gnanam6931@gmail.com>
2022-04-18 14:50:30 +0530

    First Commit

--- src/main/java/org/git/SampleCheckCode.java
+++ src/main/java/org/git/SampleCheckCode.java
@@ -1,10 +1,18 @@
 package org.git;
 
+
 public class SampleCheckCode {
 
 	
 	public static void main(String[] args) {
 		
+	
+	
+		
 		System.out.println("Welcome to Moankshra");
+		
+	
+		
+		
 	}
 }
